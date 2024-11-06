a5b1dbbdc6fb7395e177edcd98c9a8a38970f06f
Abhishek Madhukar Mahajan <abhishekmahajan3711@gmail.com>
2024-11-06 06:39:07 +0530

    Update Fib

--- LP3/GroupA_DAA/Assign1_Fibonacci_Series.py
+++ LP3/GroupA_DAA/Assign1_Fibonacci_Series.py
@@ -17,3 +17,40 @@ else:
 	step=Fib(n,0,1,1)
 	print("Step count : ",step)
 
+
+n=int(input('Enter number till which you want a fibonacci series'))
+
+fib=[]
+for i in range(n):
+    fib.append(0)
+
+for i in range(n):
+    print(fib[i])
+
+#recursive
+def fibn(i,fib):
+    if(i<=1):
+        return i
+    elif(fib[i]):
+         return fib[i]
+    else:
+        fib[i]=fibn(i-1,fib)+fibn(i-2,fib)
+        return fib[i]
+        
+fibn(n-1,fib)
+
+for i in range(n):
+    print(fib[i])
+
+#non-recursive
+
+
+# print(0)
+
+# a=0
+# b=1
+# for i in range(0,n-1):
+#     c=a+b
+#     print(c)
+#     a=b
+#     b=c
